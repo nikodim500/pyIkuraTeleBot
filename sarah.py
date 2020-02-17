c81e86e1a8227a0789d1ba7423ac12f297069995
Nikolay Chetverikov <nikolay@chetverikov.pro>
2020-02-17 22:58:10 +0300

    return loads

--- sarah.py
+++ sarah.py
@@ -50,7 +50,7 @@ def m2p():
     postdata = postdata.decode('utf-8')
     print('decoded b:')
     print(postdata)
-#    postdata = json.loads(b)
+    postdata = json.loads(b)
     sarah_logger.info('M2P. %s', postdata);
     if postdata['typ'] == 'STAT':
         to_stat(postdata['src'] + ' ' + postdata['msg'])
